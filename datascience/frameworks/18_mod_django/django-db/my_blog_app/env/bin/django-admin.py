646d3b752cce661a3d216ea8cb367332cc61ada7
gary.jackson@qlx.com <gary.jackson@qlx.com>
2020-08-20 17:16:58 -0400

    Updated R/Module 14, Frameworks/Django, Frameworks/Flask

---
+++ datascience/frameworks/18_mod_django/django-db/my_blog_app/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gsjackson/Documents/Notebooks/working_class/datascience/frameworks/18_mod_django/django-db/my_blog_app/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
